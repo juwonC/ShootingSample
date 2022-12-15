2151e9f7f6484b8c14f470fe038f0e9cddff7200
JuwonCha <juwon1512@gmail.com>
2022-12-15 23:22:10 +0900

    Add Title Widget

---
+++ Source/ShootingSample/Public/TitleWidget.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TitleWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTINGSAMPLE_API UTitleWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UTextBlock* start;
+
+	UFUNCTION()
+	void Start();
+
+protected:
+	virtual void NativeConstruct() override;
+};
